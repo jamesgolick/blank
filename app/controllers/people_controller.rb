07449f18340a288346ad74d28294de8496a866c2
James Golick <james@giraffesoft.ca>
2008-08-05 16:49:01 -0400

    r_cize people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,28 +1,10 @@
-class PeopleController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
-  
-
-  # render new.rhtml
-  def new
-    @person = Person.new
-  end
+class PeopleController < ResourceController::Base
+  actions :new, :create
  
-  def create
-    logout_keeping_session!
-    @person = Person.new(params[:person])
-    success = @person && @person.save
-    if success && @person.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      self.current_person = @person # !! now logged in
-            redirect_back_or_default(people_url)
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-    else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => 'new'
-    end
+  create do
+    before     :logout_keeping_session!
+    after      { self.current_person = @person }
+    flash      "Thanks for signing up!"
+    wants.html { redirect_back_or_default(people_url) } 
   end
 end
