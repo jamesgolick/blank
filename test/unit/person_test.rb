6f5bd596d9a91b3438a8b7149cedd333e979809f
James Golick <james@giraffesoft.ca>
2008-08-05 16:01:37 -0400

    shouldaize person test

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,102 +1,79 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PersonTest < Test::Unit::TestCase
+  def self.should_set_remember_token
+    should "set remember_token" do
+      assert_not_nil people(:james).remember_token      
+    end
+    
+    should "set remember_token_expires_at" do
+      assert_not_nil people(:james).remember_token_expires_at      
+    end
+  end
+  
   def setup
     @person = people(:james)
   end
   
-  def test_should_create_person
-    assert_difference 'Person.count' do
-      person = create_person
-      assert !person.new_record?, "#{person.errors.full_messages.to_sentence}"
-    end
-  end
+  should_require_attributes :login, :password, :password_confirmation, :email
 
-  def test_should_require_login
-    assert_no_difference 'Person.count' do
-      u = create_person(:login => nil)
-      assert u.errors.on(:login)
-    end
-  end
-
-  def test_should_require_password
-    assert_no_difference 'Person.count' do
-      u = create_person(:password => nil)
-      assert u.errors.on(:password)
-    end
-  end
-
-  def test_should_require_password_confirmation
-    assert_no_difference 'Person.count' do
-      u = create_person(:password_confirmation => nil)
-      assert u.errors.on(:password_confirmation)
-    end
-  end
-
-  def test_should_require_email
-    assert_no_difference 'Person.count' do
-      u = create_person(:email => nil)
-      assert u.errors.on(:email)
-    end
-  end
-
-  def test_should_reset_password
+  should "reset password" do
     people(:james).update_attributes(:password => 'new password', :password_confirmation => 'new password')
     assert_equal people(:james), Person.authenticate('james', 'new password')
   end
 
-  def test_should_not_rehash_password
+  should "not rehash password when it's not updated" do
     people(:james).update_attributes(:login => 'james2')
     assert_equal people(:james), Person.authenticate('james2', 'monkey')
   end
 
-  def test_should_authenticate_person
+  should "authenticate person with correct username and password" do
     assert_equal people(:james), Person.authenticate('james', 'monkey')
   end
 
-  def test_should_set_remember_token
-    people(:james).remember_me
-    assert_not_nil people(:james).remember_token
-    assert_not_nil people(:james).remember_token_expires_at
+  context "Remember me" do
+    setup do
+      @before = 2.week.from_now.utc
+      people(:james).remember_me
+      @after  = 2.week.from_now.utc
+    end
+
+    should_set_remember_token
+    
+    should "default to two weeks" do
+      assert people(:james).remember_token_expires_at.between?(@before, @after)
+    end
   end
 
-  def test_should_unset_remember_token
-    people(:james).remember_me
-    assert_not_nil people(:james).remember_token
+  should "unset remember token when asked to forget me" do
     people(:james).forget_me
     assert_nil people(:james).remember_token
   end
-
-  def test_should_remember_me_for_one_week
-    before = 1.week.from_now.utc
-    people(:james).remember_me_for 1.week
-    after = 1.week.from_now.utc
-    assert_not_nil people(:james).remember_token
-    assert_not_nil people(:james).remember_token_expires_at
-    assert people(:james).remember_token_expires_at.between?(before, after)
+  
+  context "Remember me for one week" do
+    setup do
+      @before = 1.week.from_now.utc
+      people(:james).remember_me_for 1.week
+      @after  = 1.week.from_now.utc
+    end
+    
+    should_set_remember_token
+    
+    should "set the remember token to expire a week from now" do
+      assert people(:james).remember_token_expires_at.between?(@before, @after)
+    end
   end
+  
+  context "Remember me until one week" do
+    setup do
+      @time = 1.week.from_now.utc
+      people(:james).remember_me_until @time
+    end
 
-  def test_should_remember_me_until_one_week
-    time = 1.week.from_now.utc
-    people(:james).remember_me_until time
-    assert_not_nil people(:james).remember_token
-    assert_not_nil people(:james).remember_token_expires_at
-    assert_equal people(:james).remember_token_expires_at, time
-  end
-
-  def test_should_remember_me_default_two_weeks
-    before = 2.weeks.from_now.utc
-    people(:james).remember_me
-    after = 2.weeks.from_now.utc
-    assert_not_nil people(:james).remember_token
-    assert_not_nil people(:james).remember_token_expires_at
-    assert people(:james).remember_token_expires_at.between?(before, after)
-  end
-
-protected
-  def create_person(options = {})
-    record = Person.new({ :login => 'quire', :email => 'quire@example.com', :password => 'quire69', :password_confirmation => 'quire69' }.merge(options))
-    record.save
-    record
+    should_set_remember_token
+    
+    should "set the token expire one week from now" do
+      assert_equal people(:james).remember_token_expires_at, @time
+    end
   end
 end
