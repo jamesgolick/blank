57cfbb88c621138eeacbd14eec759ef6ef8a092a
James Golick <james@giraffesoft.ca>
2008-09-22 16:40:58 -0400

    load from app/mailers

--- config/environment.rb
+++ config/environment.rb
@@ -40,7 +40,7 @@ Rails::Initializer.run do |config|
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/presenters )
+  config.load_paths += %W( #{RAILS_ROOT}/app/presenters #{RAILS_ROOT}/app/mailers )
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
