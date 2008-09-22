4824fac72e016ee916060822df71631e18a05341
James Golick <james@giraffesoft.ca>
2008-09-22 15:44:56 -0400

    add logged_in_as and not_logged_in test helpers

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -1,4 +1,8 @@
 module AuthenticatedTestHelper
+  def self.included(klass)
+    klass.send(:extend, ClassMethods)
+  end
+
   # Sets the current person in the session from the person fixtures.
   def login_as(person)
     @request.session[:person_id] = person ? people(person).id : nil
@@ -8,4 +12,25 @@ module AuthenticatedTestHelper
     @request.env["HTTP_AUTHORIZATION"] = person ? ActionController::HttpAuthentication::Basic.encode_credentials(people(person).login, 'monkey') : nil
   end
   
+  module ClassMethods
+    def logged_in_as(person)
+      context "Logged in as #{person}" do
+        setup do
+          login_as(person)
+        end
+
+        yield
+      end
+    end
+    
+    def not_logged_in
+      context "Not logged in" do
+        setup do
+          login_as(nil)
+        end
+
+        yield
+      end
+    end
+  end
 end
