9178e4673ba9b1aba7a9b9c1741ed4335e47ea24
James Golick <james@giraffesoft.ca>
2008-09-22 13:54:25 -0400

    add before? and after? methods to time for readability

---
+++ config/initializers/monkey_patches.rb
@@ -0,0 +1,1 @@
+require 'before_and_after'
---
+++ lib/before_and_after.rb
@@ -0,0 +1,14 @@
+module BeforeAndAfter
+  LEFT_SIDE_LATER  = 1
+  RIGHT_SIDE_LATER = -1
+  
+  def before?(input_time)
+    (self <=> input_time) == RIGHT_SIDE_LATER
+  end
+  
+  def after?(input_time)
+    (self <=> input_time) == LEFT_SIDE_LATER
+  end
+end
+
+Time.send :include , BeforeAndAfter
