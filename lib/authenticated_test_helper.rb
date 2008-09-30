6d426f3ee150c754f3267ae55ca50e1491a9e1c6
Mathieu Martin <mat@giraffesoft.ca>
2008-09-30 15:39:44 -0400

    Revert auth_test_helper to original Restful auth code

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -1,8 +1,4 @@
 module AuthenticatedTestHelper
-  def self.included(klass)
-    klass.send(:extend, ClassMethods)
-  end
-
   # Sets the current person in the session from the person fixtures.
   def login_as(person)
     @request.session[:person_id] = person ? people(person).id : nil
@@ -10,33 +6,5 @@ module AuthenticatedTestHelper
 
   def authorize_as(person)
     @request.env["HTTP_AUTHORIZATION"] = person ? ActionController::HttpAuthentication::Basic.encode_credentials(people(person).login, 'monkey') : nil
-  end
-  
-  module ClassMethods
-    def logged_in_as(person)
-      context "Logged in as #{person}" do
-        setup do
-          login_as(person)
-        end
-
-        yield
-      end
-    end
-    
-    def not_logged_in
-      context "Not logged in" do
-        setup do
-          login_as(nil)
-        end
-
-        yield
-      end
-    end
-    
-    def should_deny_access
-      should_respond_with :redirect
-      should_redirect_to  'login_url'
-      should_set_the_flash_to(/must be logged in/i)
-    end
   end
 end
