16a3da1226ba8348dd8c9869bb2e0dfa50bcefe5
James Golick <james@giraffesoft.ca>
2008-10-09 21:30:42 -0400

    fix routing typos

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.login     '/login',    :controller => 'sessions',    :action => 'new'
   map.logout    '/logout',   :controller => 'sessions',    :action => 'destroy'
   
-  map.register  '/register', :controller => 'peoples',     :action => 'create'
-  map.signup    '/signup',   :controller => 'peoples',     :action => 'new'
+  map.register  '/register', :controller => 'people',     :action => 'create'
+  map.signup    '/signup',   :controller => 'people',     :action => 'new'
   
   map.reset_password '/reset_password', :controller => 'password_resets', :action => 'new',    :conditions => {:method => :get}
   map.reset_password '/reset_password', :controller => 'password_resets', :action => 'create', :conditions => {:method => :post}
