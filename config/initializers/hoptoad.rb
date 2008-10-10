8fcf98ef8a60e42ca24504d4fa826ca1a7c93a06
James Golick <james@giraffesoft.ca>
2008-10-09 21:39:30 -0400

    add a log message if there's no hoptoad key

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,5 @@
 HoptoadNotifier.configure do |config|
   config.api_key = ''
 end
+
+RAILS_DEFAULT_LOGGER.warn('Please add a hoptoad key to config/initializers/hoptoad.rb.') if HoptoadNotifier.api_key.blank?
