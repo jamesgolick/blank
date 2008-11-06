858d42c9a93b7e3356e6f525392a520199c257f1
François Beausoleil <francois@teksol.info>
2008-11-05 21:38:28 -0500

    DataMapper expects symbolized keys to setup DB adapter.
    Can't user #symbolize_keys there, because ActiveRecord isn't loaded yet?

--- config/initializers/datamapper.rb
+++ config/initializers/datamapper.rb
@@ -1,3 +1,8 @@
 require "dm-core"
 hash = YAML.load(File.new(Rails.root + "/config/database.yml"))
-DataMapper.setup(:default, hash[Rails.env])
+hash = hash[Rails.env]
+hash.keys.each do |key|
+  hash[key.to_sym] = hash.delete(key)
+end
+
+DataMapper.setup(:default, hash)
