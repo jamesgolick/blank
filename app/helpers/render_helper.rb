0f8e9689c83dbbaf8d7d613465a270cebf7c3a9c
James Golick <james@giraffesoft.ca>
2008-11-08 14:12:01 -0500

    add a session toolbar when logged in

--- app/helpers/render_helper.rb
+++ app/helpers/render_helper.rb
@@ -2,4 +2,8 @@ module RenderHelper
   def render_flash
     render :partial => "shared/flash", :object => flash
   end
+
+  def render_session_toolbar
+    render :partial => 'shared/session_toolbar'
+  end
 end
