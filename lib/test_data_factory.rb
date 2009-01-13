89a788422bfa6ac135044654129c485901f217fb
François Beausoleil <francois@teksol.info>
2009-01-13 12:06:10 -0500

    TestDataFactory does not do a mass-assign, to go through attr_protected
    Else, a model with an attr_accessible or attr_protected will have some
    attributes left blank.

--- lib/test_data_factory.rb
+++ lib/test_data_factory.rb
@@ -7,17 +7,22 @@ module TestDataFactory
     define_method(:"hash_for_#{type}") do |*args|
       data.merge(args.extract_options!)
     end
-    
+
     meta_def(:"hash_for_#{type}") do |*args|
       data.merge(args.extract_options!)
     end
 
     define_method(:"create_#{type}") do |*args|
-      klass.create!(send(:"hash_for_#{type}", args.extract_options!))
+      send("build_#{type}", args.extract_options).save!
     end
 
     define_method(:"build_#{type}") do |*args|
-      klass.new(send(:"hash_for_#{type}", args.extract_options!))
+      params = send(:"hash_for_#{type}", args.extract_options!)
+      returning(klass.new) do |obj|
+        params.each do |k, v|
+          obj.send("#{k}=", v)
+        end
+      end
     end
   end
 end
