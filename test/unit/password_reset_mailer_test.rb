30699d57aebde2458a9ea63649b9bbfae1bd2234
François Beausoleil <francois@teksol.info>
2009-05-06 13:10:19 -0400

    Fixed password reset mailer for Rails 2.3.2

--- test/unit/password_reset_mailer_test.rb
+++ test/unit/password_reset_mailer_test.rb
@@ -3,17 +3,18 @@ require File.dirname(__FILE__)+'/../test_helper'
 class PasswordResetMailerTest < ActionMailer::TestCase
   context "The password reset mailer" do
     setup do
-      people(:james).create_password_reset_code
-      PasswordResetMailer.deliver_password_reset(people(:james))
+      @person = Factory(:person)
+      @person.create_password_reset_code
+      PasswordResetMailer.deliver_password_reset(@person)
       @sent = ActionMailer::Base.deliveries.first
     end
 
     should "send to that user" do
-      assert_equal [people(:james).email], @sent.to
+      assert_equal [@person.email], @sent.to
     end
     
     should "have the URL of the password reset code in it" do
-      assert_match people(:james).password_reset_code, @sent.body
+      assert_match @person.password_reset_code, @sent.body
     end
   end
 end
