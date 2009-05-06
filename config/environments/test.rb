08b0cefc6dd12386f3c5d2c88ba3b8f803a31655
François Beausoleil <francois@teksol.info>
2009-05-06 12:55:45 -0400

    Added FactoryGirl as a test dependency

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,5 +21,6 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem "thoughtbot-shoulda", :lib => "shoulda/rails", :source => "http://gems.github.com"
-config.gem "mocha",              :lib => "mocha"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl",  :source => "http://gems.github.com"
+config.gem "thoughtbot-shoulda",      :lib => "shoulda/rails", :source => "http://gems.github.com"
+config.gem "mocha",                   :lib => "mocha"
