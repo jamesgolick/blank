9dbd553c7ba75693e6704e32b4fb2de4e2883223
François Beausoleil <francois@teksol.info>
2009-05-22 10:23:48 -0400

    Updated PasswordResetsControllerTest to FactoryGirl

--- test/functional/password_resets_controller_test.rb
+++ test/functional/password_resets_controller_test.rb
@@ -10,17 +10,7 @@ class PasswordResetsControllerTest < ActionController::TestCase
     should_respond_with :success
     should_render_a_form
   end
-  
-  context "on POST to :create with a valid email" do
-    setup do
-      post :create, :email => people(:james).email
-    end
 
-    should_render_template :create
-    should_respond_with :success
-    should_set_the_flash_to(/email/i)
-  end
-  
   context "on POST to :create with an invalid email" do
     setup do
       post :create, :email => 'clearly@nouser.bythisemail.com'
@@ -29,5 +19,23 @@ class PasswordResetsControllerTest < ActionController::TestCase
     should_render_template :new
     should_respond_with :success
     should_set_the_flash_to(/cannot/i)
+  end
+
+  context "Given an existing user" do
+    setup do
+      @person = Factory(:person)
+    end
+
+    context "on POST to :create with a valid email" do
+      setup do
+        post :create, :email => @person.email
+      end
+
+      should_change "@person.reload.password_reset_code"
+      should_change "@person.reload.password_reset_code_expires"
+      should_render_template :create
+      should_respond_with :success
+      should_set_the_flash_to(/email/i)
+    end
   end
 end
