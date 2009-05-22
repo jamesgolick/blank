ac08a652986fd0e86cd9989ad57e41c19e6c4d85
François Beausoleil <francois@teksol.info>
2009-05-22 09:46:41 -0400

    Whitespace normalization

--- config/environment.rb
+++ config/environment.rb
@@ -27,12 +27,12 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   
   # Blank's default dependencies
-  config.gem "ruby-openid", :lib => "openid"
+  config.gem "ruby-openid",              :lib => "openid"
   config.gem "active_presenter"
   config.gem "andand"
   
-  config.gem "mislav-will_paginate",              :lib => "will_paginate",      :source => "http://gems.github.com"
-  config.gem "giraffesoft-attribute_fu",          :lib => "attribute_fu",       :source => "http://gems.github.com"
+  config.gem "mislav-will_paginate",     :lib => "will_paginate", :source => "http://gems.github.com"
+  config.gem "giraffesoft-attribute_fu", :lib => "attribute_fu",  :source => "http://gems.github.com"
   
   # Application-specific dependencies
   #config.gem ''
