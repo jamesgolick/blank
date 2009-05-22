4e7746ffd5a87b992ac8ac872211750338a86471
François Beausoleil <francois@teksol.info>
2009-05-22 10:19:54 -0400

    Fixed the AccountsControllerTest

--- test/functional/accounts_controller_test.rb
+++ test/functional/accounts_controller_test.rb
@@ -1,57 +1,59 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
-  def setup
-    @person = Factory(:person)
-  end
-  
-  logged_in_as :james do
-    # should_be_restful do |resource|
-    #   resource.formats         = [:html]
-    #   resource.klass           = Person
-    #   resource.actions         = [:edit, :update]
-    #   resource.update.redirect = 'account_url'
-    # end
-  end
-  
-  not_logged_in do
-    on :get, :edit, :id => lambda { @person.id } do
-      should_deny_access
+  context "" do
+    setup do
+      @person = Factory(:person)
     end
 
-    context "With valid password reset code" do
+    context "Logged in" do
       setup do
-        @person.create_password_reset_code
-        get :edit, :password_reset_code => @person.password_reset_code
+        login_as @person
       end
 
-      should_set_the_flash_to(/reset your password/i)
-      should "login as the user who owns the code" do
-        assert_equal @person, current_person
-      end
-      
-      should "clear the password reset code" do
-        assert_nil @person.reload.password_reset_code
-      end
-      
-      should_respond_with :success
-      should_render_template :edit
+      # should_be_restful do |resource|
+      #   resource.formats         = [:html]
+      #   resource.klass           = Person
+      #   resource.actions         = [:edit, :update]
+      #   resource.update.redirect = 'account_url'
+      # end
     end
-    
-    context "on GET to :edit" do
-      setup do
-        get :edit
+
+    not_logged_in do
+      context "on GET to :edit" do
+        setup do
+          get :edit, :id => @person.id
+        end
+
+        should_deny_access
       end
 
-      should_deny_access
-    end
-    
-    context "on POST to :update" do
-      setup do
-        post :update, :person => {}
+      context "With valid password reset code" do
+        setup do
+          @person.create_password_reset_code
+          get :edit, :password_reset_code => @person.password_reset_code
+        end
+
+        should_set_the_flash_to(/reset your password/i)
+        should "login as the user who owns the code" do
+          assert_equal @person, current_person
+        end
+
+        should "clear the password reset code" do
+          assert_nil @person.reload.password_reset_code
+        end
+
+        should_respond_with :success
+        should_render_template :edit
       end
 
-      should_deny_access
+      context "on PUT to :update" do
+        setup do
+          put :update, :id => @person.id, :person => {}
+        end
+
+        should_deny_access
+      end
     end
   end
 end
